5498ab8ba70986777e383ebca8826771b24c9573
Toh Yan Hui <yanhuiphone@gmail.com>
2025-08-18 12:08:41 +0800

    Add greeting and exit functionality to Momo.java

--- src/main/java/Momo.java
+++ src/main/java/Momo.java
@@ -6,6 +6,12 @@ public class Momo {
                 + "| |\\/| |/ _ \\| '_ ` _ \\ / _ \\ \n"
                 + "| |  | | (_) | | | | | | (_) |\n"
                 + "|_|  |_|\\___/|_| |_| |_|\\___/ \n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello I'm\n" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+
     }
 }
