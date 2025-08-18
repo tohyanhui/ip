aee6a621f1915677bd236cd73be100365bc2c210
Toh Yan Hui <yanhuiphone@gmail.com>
2025-08-18 11:47:56 +0800

    Rename Duke.java and update logo string to Momo

---
+++ src/main/java/Momo.java
@@ -0,0 +1,11 @@
+public class Momo {
+    public static void main(String[] args) {
+        String logo = " __  __                       \n"
+                + "|  \\/  |                      \n"
+                + "| \\  / | ___  _ __ ___   ___  \n"
+                + "| |\\/| |/ _ \\| '_ ` _ \\ / _ \\ \n"
+                + "| |  | | (_) | | | | | | (_) |\n"
+                + "|_|  |_|\\___/|_| |_| |_|\\___/ \n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
