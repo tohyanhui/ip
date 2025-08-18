ee4acce925ca1047b77ed690e7b798fe1a9c0270
Toh Yan Hui <yanhuiphone@gmail.com>
2025-08-18 18:15:42 +0800

    Add indentations to Momo's messages

--- src/main/java/Momo.java
+++ src/main/java/Momo.java
@@ -1,29 +1,40 @@
 import java.util.Scanner;
 
 public class Momo {
+    private static final String HORIZONTAL_LINE = "____________________________________________________________";
+    private static final String LOGO = " __  __                       \n"
+            + "|  \\/  |                      \n"
+            + "| \\  / | ___  _ __ ___   ___  \n"
+            + "| |\\/| |/ _ \\| '_ ` _ \\ / _ \\ \n"
+            + "| |  | | (_) | | | | | | (_) |\n"
+            + "|_|  |_|\\___/|_| |_| |_|\\___/ \n";
+    private static final String MESSAGE_GREET = "Hello I'm\n" + LOGO + "\n" + "What can I do for you?";
+    private static final String MESSAGE_BYE = "Bye. Hope to see you again soon!";
+
+
+    // Indents every line of the text
+    private static String indent(String text) {
+        return "\t" + text.replace("\n", "\n\t");
+    }
+
+    private static void printPrettyMessage(String message) {
+        System.out.println(indent(HORIZONTAL_LINE));
+        System.out.println(indent(message));
+        System.out.println(indent(HORIZONTAL_LINE));
+        System.out.println();
+    }
+
     public static void main(String[] args) {
-        String logo = " __  __                       \n"
-                + "|  \\/  |                      \n"
-                + "| \\  / | ___  _ __ ___   ___  \n"
-                + "| |\\/| |/ _ \\| '_ ` _ \\ / _ \\ \n"
-                + "| |  | | (_) | | | | | | (_) |\n"
-                + "|_|  |_|\\___/|_| |_| |_|\\___/ \n";
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello I'm\n" + logo);
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________\n");
+        printPrettyMessage(MESSAGE_GREET);
+
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         while (!input.equals("bye")) {
-            System.out.println("____________________________________________________________");
-            System.out.println(input);
-            System.out.println("____________________________________________________________\n");
+            printPrettyMessage(input);
             input = scanner.nextLine();
         }
         scanner.close();
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________\n");
 
+        printPrettyMessage(MESSAGE_BYE);
     }
 }
