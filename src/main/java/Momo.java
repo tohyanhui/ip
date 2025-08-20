fce6c3792964b815ad28c53070053e2fca4f3908
Toh Yan Hui <yanhuiphone@gmail.com>
2025-08-21 00:57:05 +0800

    Refactor: Extract TaskList from Momo

--- src/main/java/Momo.java
+++ src/main/java/Momo.java
@@ -13,7 +13,7 @@ public class Momo {
             + "|_|  |_|\\___/|_| |_| |_|\\___/\n";
     private static final String MESSAGE_GREET = "Hello I'm\n" + LOGO + "\n" + "What can I do for you?";
     private static final String MESSAGE_BYE = "Bye. Hope to see you again soon!";
-    private final ArrayList<Task> tasks = new ArrayList<>();
+    private final TaskList tasks = new TaskList();
 
     // Indents every line of the text
     private static String indent(String text) {
@@ -32,38 +32,14 @@ public class Momo {
         System.out.println();
     }
 
-    private void addTask(Task task) {
-        tasks.add(task);
-    }
-
-    private Task deleteTask(int index) {
-        return tasks.remove(index);
-    }
-    
-    private Task getTask(int index) {
-        return tasks.get(index);
-    }
-    
-    private void markTask(int index) {
-        getTask(index).markAsDone();
-    }
-
-    private void unmarkTask(int index) {
-        getTask(index).unmarkFromDone();
-    }
-    
-    private int computeNumTasks() {
-        return tasks.size();
-    }
-
     private String createAddTaskMessage(Task task) {
         return "Got it. I've added this task:\n" +  space(space(task.toString())) 
-                + "\nNow you have " + computeNumTasks() + " tasks in the list.";
+                + "\nNow you have " + tasks.size() + " tasks in the list.";
     }
 
     private String createDeleteTaskMessage(Task task) {
         return "Noted. I've removed this task:\n" +  space(space(task.toString()))
-                + "\nNow you have " + computeNumTasks() + " tasks in the list.";
+                + "\nNow you have " + tasks.size() + " tasks in the list.";
     }
 
     public static void main(String[] args) {
@@ -75,9 +51,8 @@ public class Momo {
         while (!input.equals("bye")) {
             try {
                 if (input.equals("list")) {
-                    int numTasks = momo.computeNumTasks();
-                    String listMessage = IntStream.range(0, numTasks)
-                            .mapToObj(x -> String.format("%d.%s", x + 1, momo.getTask(x).toString()))
+                    String listMessage = IntStream.range(0, momo.tasks.size())
+                            .mapToObj(x -> String.format("%d.%s", x + 1, momo.tasks.getTask(x).toString()))
                             .collect(Collectors.joining("\n"));
                     printPrettyMessage("Here are the tasks in your list:\n" + listMessage);
                 } else if (input.equals("delete")) {
@@ -87,7 +62,7 @@ public class Momo {
                 } else if (input.startsWith("delete ")) {
                     try {
                         int index = Integer.parseInt(input.split(" ")[1]) - 1;
-                        Task deletedTask = momo.deleteTask(index);
+                        Task deletedTask = momo.tasks.deleteTask(index);
                         printPrettyMessage(momo.createDeleteTaskMessage(deletedTask));
                     } catch (NumberFormatException e) {
                         String errorDetail = "The task number provided is not an integer!";
@@ -101,8 +76,8 @@ public class Momo {
                 } else if (input.startsWith("mark ")) {
                     try {
                         int index = Integer.parseInt(input.split(" ")[1]) - 1;
-                        momo.markTask(index);
-                        printPrettyMessage("Nice! I've marked this task as done:\n  " + momo.getTask(index).toString());
+                        momo.tasks.markTask(index);
+                        printPrettyMessage("Nice! I've marked this task as done:\n  " + momo.tasks.getTask(index).toString());
                     } catch (NumberFormatException e) {
                         String errorDetail = "The task number provided is not an integer!";
                         String errorFix = "Fix: Try \"mark <integer>\" instead!";
@@ -115,9 +90,9 @@ public class Momo {
                 } else if (input.startsWith("unmark ")) {
                     try {
                         int index = Integer.parseInt(input.split(" ")[1]) - 1;
-                        momo.unmarkTask(index);
+                        momo.tasks.unmarkTask(index);
                         printPrettyMessage("OK, I've marked this task as not done yet:\n  " 
-                                + momo.getTask(index).toString());
+                                + momo.tasks.getTask(index).toString());
                     } catch (NumberFormatException e) {
                         String errorDetail = "The task number provided is not an integer!";
                         String errorFix = "Fix: Try \"unmark <integer>\" instead!";
@@ -130,7 +105,7 @@ public class Momo {
                 } else if (input.startsWith("todo ")) {
                     String description = input.substring(5);
                     Task task = new Todo(description);
-                    momo.addTask(task);
+                    momo.tasks.addTask(task);
                     printPrettyMessage(momo.createAddTaskMessage(task));
                 } else if (input.equals("deadline")) {
                     String errorDetail = "The description of the deadline is empty!";
@@ -140,7 +115,7 @@ public class Momo {
                     String[] parsedInput = input.substring(9).split(" /by ");
                     try {
                         Task task = new Deadline(parsedInput[0], parsedInput[1]);
-                        momo.addTask(task);
+                        momo.tasks.addTask(task);
                         printPrettyMessage(momo.createAddTaskMessage(task));
                     } catch (ArrayIndexOutOfBoundsException e) {
                         String errorDetail = "The deadline is missing \"/by\"!";
@@ -156,7 +131,7 @@ public class Momo {
                     try {
                         String[] parsedStartEndTime = parsedInput[1].split(" /to ");
                         Task task = new Event(parsedInput[0], parsedStartEndTime[0], parsedStartEndTime[1]);
-                        momo.addTask(task);
+                        momo.tasks.addTask(task);
                         printPrettyMessage(momo.createAddTaskMessage(task));
                     } catch (ArrayIndexOutOfBoundsException e) {
                         String errorDetail = "The event is missing \"/from\" or \"/to\"!";
---
+++ src/main/java/TaskList.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class TaskList {
+    private final ArrayList<Task> tasks = new ArrayList<>();
+    
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public Task deleteTask(int index) {
+        return tasks.remove(index);
+    }
+
+    public Task getTask(int index) {
+        return tasks.get(index);
+    }
+
+    public void markTask(int index) {
+        getTask(index).markAsDone();
+    }
+
+    public void unmarkTask(int index) {
+        getTask(index).unmarkFromDone();
+    }
+
+    public int size() {
+        return tasks.size();
+    }
+}
