0f4af0d8a2af09086ebd05c7ba6fac230513dd17
Toh Yan Hui <yanhuiphone@gmail.com>
2025-08-20 20:47:04 +0800

    Use ArrayList<Task> to store tasks

--- src/main/java/Momo.java
+++ src/main/java/Momo.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -12,9 +13,7 @@ public class Momo {
             + "|_|  |_|\\___/|_| |_| |_|\\___/\n";
     private static final String MESSAGE_GREET = "Hello I'm\n" + LOGO + "\n" + "What can I do for you?";
     private static final String MESSAGE_BYE = "Bye. Hope to see you again soon!";
-    private final Task[] tasks = new Task[100];
-    private int numTasks = 0;
-
+    private final ArrayList<Task> tasks = new ArrayList<>();
 
     // Indents every line of the text
     private static String indent(String text) {
@@ -34,21 +33,28 @@ public class Momo {
     }
 
     private void addTask(Task task) {
-        tasks[numTasks] = task;
-        numTasks++;
+        tasks.add(task);
     }
-
+    
+    private Task getTask(int index) {
+        return tasks.get(index);
+    }
+    
     private void markTask(int index) {
-        tasks[index].markAsDone();
+        getTask(index).markAsDone();
     }
 
     private void unmarkTask(int index) {
-        tasks[index].unmarkFromDone();
+        getTask(index).unmarkFromDone();
+    }
+    
+    private int computeNumTasks() {
+        return tasks.size();
     }
 
     private String createAddTaskMessage(Task task) {
         return "Got it. I've added this task:\n" +  space(space(task.toString())) 
-                + "\nNow you have " + this.numTasks + " tasks in the list.";
+                + "\nNow you have " + computeNumTasks() + " tasks in the list.";
     }
 
     public static void main(String[] args) {
@@ -60,10 +66,9 @@ public class Momo {
         while (!input.equals("bye")) {
             try {
                 if (input.equals("list")) {
-                    int numTasks = momo.numTasks;
-                    Task[] tasks = momo.tasks;
+                    int numTasks = momo.computeNumTasks();
                     String listMessage = IntStream.range(0, numTasks)
-                            .mapToObj(x -> String.format("%d.%s", x + 1, tasks[x].toString()))
+                            .mapToObj(x -> String.format("%d.%s", x + 1, momo.getTask(x).toString()))
                             .collect(Collectors.joining("\n"));
                     printPrettyMessage("Here are the tasks in your list:\n" + listMessage);
                 } else if (input.equals("mark")) {
@@ -74,7 +79,7 @@ public class Momo {
                     try {
                         int index = Integer.parseInt(input.split(" ")[1]) - 1;
                         momo.markTask(index);
-                        printPrettyMessage("Nice! I've marked this task as done:\n  " + momo.tasks[index].toString());
+                        printPrettyMessage("Nice! I've marked this task as done:\n  " + momo.getTask(index).toString());
                     } catch (NumberFormatException e) {
                         String errorDetail = "The task number provided is not an integer!";
                         String errorFix = "Fix: Try \"mark <integer>\" instead!";
@@ -89,7 +94,7 @@ public class Momo {
                         int index = Integer.parseInt(input.split(" ")[1]) - 1;
                         momo.unmarkTask(index);
                         printPrettyMessage("OK, I've marked this task as not done yet:\n  " 
-                                + momo.tasks[index].toString());
+                                + momo.getTask(index).toString());
                     } catch (NumberFormatException e) {
                         String errorDetail = "The task number provided is not an integer!";
                         String errorFix = "Fix: Try \"unmark <integer>\" instead!";
