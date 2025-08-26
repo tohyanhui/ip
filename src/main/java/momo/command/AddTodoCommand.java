6497774490b6bc281a4009284cba8880d942ad0a
Toh Yan Hui <yanhuiphone@gmail.com>
2025-08-27 01:05:57 +0800

    Add JavaDoc to command classes

--- src/main/java/momo/command/AddTodoCommand.java
+++ src/main/java/momo/command/AddTodoCommand.java
@@ -6,13 +6,31 @@ import momo.task.TaskList;
 import momo.task.Todo;
 import momo.ui.Ui;
 
+/**
+ * Represents a command that adds a {@link Todo} task
+ * to the task list.
+ */
 public class AddTodoCommand implements Command {
     private final String description;
-    
+
+    /**
+     * Creates a new {@code AddTodoCommand}.
+     *
+     * @param description the description of the todo task.
+     */
     public AddTodoCommand(String description) {
         this.description = description;
     }
-    
+
+    /**
+     * Adds a new {@link Todo} task to the task list.
+     * Also saves the updated task list to storage and
+     * displays a confirmation message to the user.
+     *
+     * @param tasks the task list to which the todo is added.
+     * @param ui the user interface that displays messages to the user.
+     * @param storage the storage handler that saves the updated task list.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
         Task task = new Todo(description);
@@ -20,7 +38,12 @@ public class AddTodoCommand implements Command {
         storage.save(tasks);
         ui.printAddTaskMessage(task, tasks);
     }
-    
+
+    /**
+     * Returns whether this command will exit the program.
+     *
+     * @return {@code false}, as adding a todo does not exit the program.
+     */
     @Override
     public boolean isExit() {
         return false;
