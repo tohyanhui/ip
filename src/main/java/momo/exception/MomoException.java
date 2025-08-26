374b3d9f1a23bb9830f80fb0ab210e0b90faa21e
Toh Yan Hui <yanhuiphone@gmail.com>
2025-08-27 01:15:48 +0800

    Add JavaDoc to MomoException

--- src/main/java/momo/exception/MomoException.java
+++ src/main/java/momo/exception/MomoException.java
@@ -1,6 +1,16 @@
 package momo.exception;
 
+/**
+ * Represents a custom exception used in the Momo application.
+ * Prepends "Nani?!? " to the provided error message.
+ */
 public class MomoException extends Exception {
+
+    /**
+     * Creates a new {@code MomoException} with the specified detail message.
+     *
+     * @param message the detail message for this exception.
+     */
     public MomoException(String message) {
         super("Nani?!? " + message);
     }
